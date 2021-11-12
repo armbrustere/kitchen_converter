23f94ac7dfe729bca06862e4eae670be231f9596
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-11-11 20:30:23 -0500

    Adds the ability to convert from lb to kg.

--- app/src/main/java/com/ethan/measurement_converter/Conversion_Fragment.java
+++ app/src/main/java/com/ethan/measurement_converter/Conversion_Fragment.java
@@ -3,10 +3,11 @@ package com.ethan.measurement_converter;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -19,6 +20,12 @@ public class Conversion_Fragment extends Fragment {
     private Spinner unit_from;
     private TextView result;
     private Spinner unit_to;
+    private Conversion_Unit cu;
+
+
+    public Conversion_Fragment() {
+        cu = new Conversion_Unit();
+    }
 
 
     //Overrides the Conversion_Activity onCreate method
@@ -32,58 +39,63 @@ public class Conversion_Fragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_conv, container, false);
 
         //Add references to elements
-        convert_from_text = (EditText) v.findViewById(R.id.convert_from);
-        result = (TextView) v.findViewById(R.id.result);
-        convert_from_text.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        convert_from_text = v.findViewById(R.id.convert_from);
+        result = v.findViewById(R.id.result);
+        unit_from = v.findViewById(R.id.unit_from);
+        unit_to = v.findViewById(R.id.unit_to);
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                //Placeholder here add value to something for conversion
-                //probably need to convert from string to int
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-        unit_from = (Spinner) v.findViewById(R.id.unit_from);
-        unit_to = (Spinner) v.findViewById(R.id.unit_to);
+        //Add adapters to spinners for units
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(),
                 R.array.units_of_measurement, android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         unit_from.setAdapter(adapter);
         unit_to.setAdapter(adapter);
-//        unit_from.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//
-//            }
-//        });
+
+        //Add listeners to elements
+        convert_from_text.setOnEditorActionListener(this::onEditorAction);
 
 
         return v;
     }
 
-    private void convert()
-    {
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        int keyCode = -1;
+        double kgtoLB = cu.getKg_perLb();
+        if (event != null) {
+            keyCode = event.getKeyCode();
+        }
+        if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_UNSPECIFIED || keyCode == KeyEvent.KEYCODE_ENTER) {
+            convert();
+//            System.out.println("--------------------------------");
+//            System.out.println("THe enter key is PRESSSEDDD!!!!");
+//            System.out.println(kgtoLB);
+//            System.out.println("--------------------------------");
+
+        }
+        return false;
+    }
+
+    private void convert() {
         String from_numString = convert_from_text.getText().toString();
         double from_numDoub;
-        if(from_numString.equals(""))
-        {
+        double doubResult;
+        double kgtoLB;
+        String test;
+
+        if (from_numString.equals("")) {
             from_numDoub = 0;
-        }else
-        {
+        } else {
             from_numDoub = Double.parseDouble(from_numString);
+        }
+
+
+        //Convert units
+        if (unit_from.getSelectedItem().toString().equals("lb") && unit_to.getSelectedItem().toString().equals("kg")) {
+            kgtoLB = cu.getKg_perLb();
+            doubResult = (from_numDoub * kgtoLB);
+            test = String.format(String.valueOf(doubResult), "%.2f");
+            result.setText(test);
+           // result.setText(doubResult.f);
 
         }
     }
---
+++ app/src/main/java/com/ethan/measurement_converter/Conversion_Unit.java
@@ -0,0 +1,14 @@
+package com.ethan.measurement_converter;
+
+public class Conversion_Unit {
+    public double kg_perLb;
+
+
+    public Conversion_Unit() {
+        kg_perLb = 0.45359237;
+
+    }
+
+    public double getKg_perLb() {return kg_perLb; }
+
+}
