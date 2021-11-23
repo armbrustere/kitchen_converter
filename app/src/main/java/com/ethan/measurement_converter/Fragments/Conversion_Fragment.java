51a1683b4d6c49cced21361a48ce43d8c307d372
Ethan Armbruster <ethan.armbruster01@gmail.com>
2021-11-23 17:17:23 -0500

    Adds fraction buttons, the proper layout, and a new logo for the app(kind of temporary)

--- app/src/main/java/com/ethan/measurement_converter/Fragments/Conversion_Fragment.java
+++ app/src/main/java/com/ethan/measurement_converter/Fragments/Conversion_Fragment.java
@@ -3,15 +3,15 @@ package com.ethan.measurement_converter.Fragments;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
 
 import androidx.fragment.app.Fragment;
 
@@ -30,6 +30,9 @@ public class Conversion_Fragment extends Fragment {
     private EditText amount_from;
     private TextView amount_to;
     private Spinner unit_from, unit_to;
+    private Button F1_4, F1_3, F1_2;
+    DecimalFormat df;
+    private View v;
 
     //Overrides the Conversion_Activity onCreate method
     @Override
@@ -40,30 +43,20 @@ public class Conversion_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_conv, container, false);
+        v = inflater.inflate(R.layout.fragment_conv, container, false);
+        df = new DecimalFormat("###.##");
         //Add references to elements
         amount_from = v.findViewById(R.id.amount1);
         amount_to = v.findViewById(R.id.amount2);
         unit_from = v.findViewById(R.id.unit_from);
         unit_to = v.findViewById(R.id.unit_to);
-
+        F1_2 = v.findViewById(R.id.Fraction1_2);
+        F1_3 = v.findViewById(R.id.Fraction1_3);
+        F1_4 = v.findViewById(R.id.Fraction1_4);
         setupArrayAdapters();
         setupEditListeners();
-
-
+        setupFractionButtons();
         return v;
-    }
-
-
-    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-        int keyCode = -1;
-        if (event != null) {
-            keyCode = event.getKeyCode();
-        }
-        if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_UNSPECIFIED || keyCode == KeyEvent.KEYCODE_ENTER) {
-            convert();
-        }
-        return false;
     }
 
 
@@ -74,12 +67,12 @@ public class Conversion_Fragment extends Fragment {
 
         //Gets the amount from the edittext element and sets up a formatter for rounding
         String fromString = amount_from.getText().toString();
-        DecimalFormat df = new DecimalFormat("###.##");
+
 
         double a_from;
 
         //Accounts for startup when no Input has been entered
-        if (fromString.equals("")) {
+        if (fromString.equals("") || fromString.equals(".")) {
             a_from = 0;
         } else {
             a_from = Double.parseDouble(fromString);
@@ -107,8 +100,14 @@ public class Conversion_Fragment extends Fragment {
         unit_to.setAdapter(unitAdapter);
     }
 
+    private void setupFractionButtons() {
+        setupFractionListener(F1_2.getId(), 0.50f);
+        setupFractionListener(F1_3.getId(), 0.33f);
+        setupFractionListener(F1_4.getId(), 0.25f);
+    }
+
     private void setupEditListeners() {
-        //Add listeners to elements
+        //Add listeners to edittext and spinners
         amount_from.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -126,7 +125,7 @@ public class Conversion_Fragment extends Fragment {
 
             }
         });
-        amount_from.setOnEditorActionListener(this::onEditorAction);
+
         unit_from.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -149,8 +148,25 @@ public class Conversion_Fragment extends Fragment {
 
             }
         });
+    }
+
+
+    private void setupFractionListener(final int resId, final float frac_val) {
+        Button Fbutton = v.findViewById(resId);
+        Fbutton.setOnClickListener(v -> addFraction(frac_val));
+    }
+
+    private void addFraction(final float frac_value) {
+        String amount_String = amount_from.getText().toString();
+        double val = amount_String.isEmpty() ? 0 : Double.parseDouble(amount_String);
+        val += frac_value;
+        //Fixes issues when using ⅓ button
+        double fraction = val % 1;
+        if (fraction <= 0.01 || fraction >= 0.99) {
+            val = Math.round(val);
+        }
+        amount_from.setText(df.format(val));
 
 
     }
-
 }
